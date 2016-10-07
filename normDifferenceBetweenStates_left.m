0809718766f0125f8f2abb4702959579d2829b3b
Vid Stojevic <vid33@me.com>
2016-10-07 01:25:12 +0100

    Cleaned up version

--- normDifferenceBetweenStates_left.m
+++
@@ -1,8 +0,0 @@
-function [ out ] = normDifferenceBetweenStates_left( A, B )
-
-    out = overlap_A_B_left(A, A) - overlap_A_B_left(A, B) - overlap_A_B_left(B, A) ...
-            + overlap_A_B_left(B, B);
-
-
-end
-
